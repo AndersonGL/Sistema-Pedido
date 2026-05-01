64478f281ea2a58f6db6dff0cecfa154cd53df99
Anderson Lignelli <lignelli77@gmail.com>
2026-05-01 00:06:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { OrderCard } from './components/OrderCard'
+import './index.css'
+
+export const App = () => {
+  return (
+    <div className='container'>
+
+
+{  /* Primeiro componente- Container do pedido - Container - Card */}
+
+<OrderCard/>
+
+    </div>
+  )
+}
+
---
+++ src/components/OrderCard.jsx
@@ -0,0 +1,13 @@
+import "./OrderCard.css"
+import { FaClipboardList } from "react-icons/fa"
+
+
+export function OrderCard()  {
+  return (
+    <h2>
+        <FaClipboardList />
+        Pedidos do Dia
+    </h2>
+  )
+}
+
